3c22c3d14614dc38bd1a3b156a3e94d0b1ef78f9
rittichai.hiranlikit <rittichai.hiranlikit@IT1-7408000000.itoneco.com>
2015-08-09 23:06:54 +0700

    edit new project scan screen

--- src/TSProj/ProductBundle/Controller/NewProjectController.php
+++ src/TSProj/ProductBundle/Controller/NewProjectController.php
@@ -19,5 +19,62 @@ class NewProjectController extends Controller
         );
         
     }
+    
+    /**
+     * @Route("/empdetail",name="ajax_get_employee_detail")
+     */
+    public function ajax_employee_detailAction()
+    {
+        $request = $this->container->get('request');       
+        $empid = $request->request->get('empid');
+        $em = $this->getDoctrine()->getEntityManager();
+        $employee = $em->getRepository("TSProjPeopleBundle:Employee")->find($empid);
+        if(count($employee)==1){
+            $response = array("code" => 100, "success" => true,"name"=>$employee->getEmployeeName(),"surname"=>$employee->getEmployeeSurname());
+        }
+        else
+        {
+         $response = array("code" => 300, "success" => true,"name"=>"","surname"=>"","message"=>"no data found");
+        }
+        return new Response(json_encode($response)); 
+    }
+    
+    /**
+     * @Route("/projectdetail",name="ajax_get_project_detail")
+     */
+    public function ajax_project_detailAction()
+    {
+        $request = $this->container->get('request');       
+        $empid = $request->request->get('empid');
+        $em = $this->getDoctrine()->getEntityManager();
+        $employee = $em->getRepository("TSProjPeopleBundle:Employee")->find($empid);
+        if(count($employee)==1){
+            $response = array("code" => 100, "success" => true,"name"=>$employee->getEmployeeName(),"surname"=>$employee->getEmployeeSurname());
+        }
+        else
+        {
+         $response = array("code" => 300, "success" => true,"name"=>"","surname"=>"","message"=>"no data found");
+        }
+        return new Response(json_encode($response)); 
+    }
+    
+    /**
+     * @Route("/processdetail",name="ajax_get_process_detail")
+     */
+    public function ajax_process_detailAction()
+    {
+        $request = $this->container->get('request');       
+        $empid = $request->request->get('empid');
+        $em = $this->getDoctrine()->getEntityManager();
+        $employee = $em->getRepository("TSProjPeopleBundle:Employee")->find($empid);
+        if(count($employee)==1){
+            $response = array("code" => 100, "success" => true,"name"=>$employee->getEmployeeName(),"surname"=>$employee->getEmployeeSurname());
+        }
+        else
+        {
+         $response = array("code" => 300, "success" => true,"name"=>"","surname"=>"","message"=>"no data found");
+        }
+        return new Response(json_encode($response)); 
+    }
 
 }
