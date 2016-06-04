b905e636ca481b6625774a426e0d0fa146b17fb8
Mint <mint_cpcu@hotmail.com>
2016-06-04 18:48:35 +0700

    stock frontend

--- src/TSProj/ProductBundle/Controller/NewProjectController.php
+++ src/TSProj/ProductBundle/Controller/NewProjectController.php
@@ -67,18 +67,22 @@ class NewProjectController extends BaseController
 //        $productStartDate = date_format($productStartDateTemp[0]->getStartDateTime(), 'Y-m-d');
         
         if(count($product)==1){
-            $response = array(  "code" => 100, 
-                                "success" => true,
-                                "productname"=>$product->getProductName(),
-                                "projectid"=>$product->getProject()->getId(),
-                                "projectname"=>$product->getProject()->getProjectName(),
-                                "expectdate"=>$product->getProject()->getExpectedDeliveryDate(),
-                                "clientname"=>$product->getProject()->getClient()->getClientName(),
-                                "productstartdate"=>date_format($product->getStartDateTime(),'Y-m-d \TH:i:s'),
-                                "projectpercent"=>$product->getProject()->getPercentFinished(),
-                                "itemcount"=>$product->getProject()->getAmount(),
+            if($product->getStock()){
+                $response = array("code" => 300, "success" => true,"message"=>"ชิ้นงานนี้ผูกกับสต๊อก ".$product->getStock()->getStockProductName()." แล้ว หากต้องการยกเลิกโปรดติดต่อ admin");
+            }else{
+                $response = array(  "code" => 100, 
+                                    "success" => true,
+                                    "productname"=>$product->getProductName(),
+                                    "projectid"=>$product->getProject()->getId(),
+                                    "projectname"=>$product->getProject()->getProjectName(),
+                                    "expectdate"=>$product->getProject()->getExpectedDeliveryDate(),
+                                    "clientname"=>$product->getProject()->getClient()->getClientName(),
+                                    "productstartdate"=>date_format($product->getStartDateTime(),'Y-m-d \TH:i:s'),
+                                    "projectpercent"=>$product->getProject()->getPercentFinished(),
+                                    "itemcount"=>$product->getProject()->getAmount(),
 
-                    );
+                        );
+            }   
         }
         else
         {
@@ -114,7 +118,7 @@ class NewProjectController extends BaseController
     {
         
         $request = $this->container->get('request');  
-        
+        $message = "";
         $productid= $request->request->get('productid'); 
         $projectid= $request->request->get('projectid');    
         $projectname= $request->request->get('projectname');    
@@ -134,7 +138,7 @@ class NewProjectController extends BaseController
        
         $now   = new \DateTime();
         $curr = $now->format("Y-m-d");
-        $day =new \DateTime($curr);
+        $timeConsum = 0;
         
         $em = $this->getDoctrine()->getEntityManager();
         $qb = $em->createQueryBuilder();
@@ -166,25 +170,23 @@ class NewProjectController extends BaseController
                ->innerJoin('ppt.product', 'pd')      
                ->innerJoin('ppt.process', 'pc')      
                ->innerJoin('ppt.employee', 'emp')      
-               ->Where('ppt.startDateTime = :startdate') 
-               ->andwhere('pd.id = :productid')     
+               ->where('pd.id = :productid')     
                ->andWhere('pc.id = :process_id')        
                ->andWhere('emp.id = :employeeid')      
-               ->andWhere('ppt.endDateTime = :null')   
+               ->andWhere('ppt.endDateTime is NULL')      
                ->setParameter('productid', $product_id)
                ->setParameter('process_id', $process_id) 
-               ->setParameter('employeeid', $emp_id) 
-               ->setParameter('endDate', null);
+               ->setParameter('employeeid', $emp_id);
             $ProductProcessTime = $qb->getQuery()->getResult();
 
-            if(!$ProductProcessTime)
-            {
+            if($ProductProcessTime==null)
+            { 
                 //insert new record
                 $newProductProcessTime = new ProductProcessTime();
                 $newProductProcessTime->setProduct($product);
                 $newProductProcessTime->setProcess($process);
                 $newProductProcessTime->setEmployee($employee);
-                $newProductProcessTime->setStartDateTime($day);
+                $newProductProcessTime->setStartDateTime($now);
                 $newProductProcessTime->setTimeConsuming('0');
                 $newProductProcessTime->setFinishedFlag('0');
                 $newProductProcessTime->setLastMaintDateTime($now);
@@ -199,32 +201,223 @@ class NewProjectController extends BaseController
                 $code = 100;
             }
             else
-            {    
-                if(count($ProductProcessTime == 1)){
-                    $ProductProcessTime->setEndDateTime($now);
-                    //calculate time consuming her and do not forget to deduct break time at noon
-                    $em->persist($ProductProcessTime);
-                    $em->flush();
-                    $code = 100;
+            {    $cnt=count($ProductProcessTime);
+                if($cnt == 1)
+                    {
+                    $ProductProcessTime = $ProductProcessTime[0];
+                    $start = $ProductProcessTime->getStartDateTime();
+                    $processDate = $start->format("Y-m-d");
+                    if($processDate == $curr){
+                        $ProductProcessTime->setEndDateTime($now);
+                        $ProductProcessTime->setLastMaintDateTime($now);
+                        //calculate Time consuming
+                        $diff = $now->diff($start);
+
+                        $timeConsum = $diff->format('%h')*60+$diff->format('%i');
+                        $ProductProcessTime->setTimeConsuming($timeConsum);
+                       
+                        $em->persist($ProductProcessTime);
+                        $em->flush();
+                        $code = 100;
+                    }else{
+                        $code = 300;
+                        $message = "การสแกนครั้งนี้ไม่ถูกต้อง - สแกนข้ามวัน";
+                    }
                 }else{
                     $code = 300; 
-                    $message = "การแสกนครั้งนี้ไม่ถูกต้อง กรุณาตรวจสอบ ProductProcessTime Admin อีกครั้ง";
+                    $message = "การสแกนครั้งนี้ไม่ถูกต้อง กรุณาติดต่อ Admin";
                 }
             }
         }
         
-          $response = array("code" => $code, 
-                  "success" => true,
-                  "message" => $message,
-                  "empname"=>"Hello",
-                  "product_id"=>$product_id,
-                  "process_id"=>$process_id,
-                  "now"=>$now,
-                  "curr"=>$curr,
-                  "ProductProcessTimeId"=>$ProductProcessTimeId,
-                  "emp_id"=>$emp_id);
-          return new Response(json_encode($response)); 
+        
+        
+        $response = array("code" => $code, 
+                "success" => true,
+                "message" => $message
+//                  "empname"=>"Hello",
+//                  "product_id"=>$product_id,
+//                  "process_id"=>$process_id,
+//                  "now"=>$now,
+//                  "curr"=>$curr,
+//                  "ProductProcessTimeId"=>$ProductProcessTime->getId(),
+//                  "emp_id"=>$emp_id
+                );
+        return new Response(json_encode($response)); 
     }
     
+    public function updateProductProject($product_id){
+
+        $em = $this->getDoctrine()->getEntityManager();
+        //count number of product process time
+        $qb = $em->createQueryBuilder();
+        $qb->select('p.id')
+           ->from('TSProjProductBundle:ProductProcessTime','p')
+           ->where('p.product = :productId')
+           ->setParameter('productId',$product_id);
+        $ppt = $qb->getQuery()->getResult(); 
+        $cntPpt = count($ppt);
+
+        $product = $em->getRepository("TSProjProductBundle:Product")->find($product_id);
+        $ppts = $product->getProductProcessTime();
+        if($cntPpt > 0){
+            
+        $TimeConsuming = 0;
+        
+            foreach($ppts as $item){
+                $TimeConsumingA = $item->getTimeConsuming();
+                $TimeConsuming = $TimeConsuming + $TimeConsumingA;
+
+            }
+
+        $result = $this->timeConsumingCalculation($TimeConsuming);
+        $product->setProductTimeConsumingDays($result[0]);
+        $product->setProductTimeConsumingHours($result[1]);
+        $product->setProductTimeConsumingMins($result[2]);
+        
+        $esDay = $product->getEstimatedTimeDay();
+        $esHour = $product->getEstimatedTimeHour();
+        $esMin = $product->getEstimatedTimeMin();
+        
+        //count distinct number of processes that have been finished
+        
+        $qb2 = $em->createQueryBuilder();
+        $qb2->select('count( distinct p.process)')
+           ->from('TSProjProductBundle:ProductProcessTime','p')
+           ->innerJoin('p.product', 'pd')      
+           ->where('pd.id = :productId') 
+           ->andWhere('p.finishedFlag = 1')     
+           ->setParameter('productId',$product_id);
+        $finishedCount = $qb2->getQuery()->getSingleScalarResult(); 
+        
+        //check whether all processes have been finished or not
+        
+        $noOfProcess =  $product->getNoOfProcess();
+        $ProductStatus = $product->getProductStatus();
+        
+        
+        $finished = 0;
+        
+        if ($ProductStatus == 'เสร็จสิ้น')
+        {
+            $finished = 1;
+        }     
+        elseif($finishedCount ==  $noOfProcess)
+        {
+            $finished = 1;
+            
+            //update product status when all processes are finished
+            $product->setProductStatus('เสร็จสิ้น');
+        }
+         
+        $resultPercent = $this->percentFinishedCalculation($TimeConsuming, $esDay, $esHour, $esMin, $finished);
+        $product-> setPercentFinished($resultPercent);
+        
+
+        //update product start date and end date time
+       
+        $qb3 = $em->createQueryBuilder();
+        $qb3->select('p')
+           ->from('TSProjProductBundle:ProductProcessTime','p')
+           ->innerJoin('p.product', 'pd')      
+           ->where('pd.id = :productId')     
+           ->setParameter('productId',$product_id)
+           ->orderBy('p.startDateTime');  
+        $products = $qb3->getQuery()->getResult();
+        $product->setStartDateTime($products[0]->getStartDateTime());
+        
+        $qb4 = $em->createQueryBuilder();
+        $qb4->select('p')
+           ->from('TSProjProductBundle:ProductProcessTime','p')
+           ->innerJoin('p.product', 'pd')      
+           ->where('pd.id = :productId')     
+           ->setParameter('productId', $product_id)
+           ->orderBy('p.endDateTime', 'desc');  
+        $products = $qb4->getQuery()->getResult();
+        $product->setEndDateTime($products[0]->getEndDateTime());
+        
+        //Sum project time consuming       
+        
+        $qb5 = $em->createQueryBuilder();
+        $qb5->select('pj.id')
+           ->from('TSProjProductBundle:Product','pd')
+           ->innerJoin('pd.project', 'pj')   
+           ->where('pd.id = :productId')     
+           ->setParameter('productId', $product_id);
+        $projectId = $qb5->getQuery()->getSingleScalarResult(); 
+        
+        $project = $em->getRepository("TSProjProductBundle:Project")->find($projectId);//insert id product into ()
+        
+        $pdts = $project->getProduct();
+        $projTimeConsuming = 0;
+        $Consume_Day = 0;
+        $Consume_Hour = 0;
+        $Consume_Min = 0;
+        $projEsDay = 0;
+        $projEsHour = 0;
+        $projEsMin = 0;
+                
+        foreach($pdts as $item2){
+
+                $Consume_Day = $item2->getProductTimeConsumingDays();
+                $Consume_Hour = $item2->getProductTimeConsumingHours();
+                $Consume_Min = $item2->getProductTimeConsumingMins();
+                
+                $projEsDay = $item2->getEstimatedTimeDay();
+                $projEsHour =  $item2->getEstimatedTimeHour();
+                $projEsMin =  $item2->getEstimatedTimeMin();
+                
+                $projTimeConsuming = $projTimeConsuming + ($Consume_Day * 24 * 60) + ($Consume_Hour * 60) + $Consume_Min;
+        }
+
+        $resultProj = $this->timeConsumingCalculation($projTimeConsuming);
+        $project->setTimeConsumingDays($resultProj[0]);
+        $project->setTimeConsumingHours($resultProj[1]);
+        $project->setTimeConsumingMins($resultProj[2]);
+        
+        //Count number of products in this project
+        
+        $qb6 = $em->createQueryBuilder();
+        $qb6->select('count(distinct pd.id)')
+           ->from('TSProjProductBundle:Product','pd')
+           ->innerJoin('pd.project', 'pj')   
+           ->where('pj.id = :projectId')     
+           ->setParameter('projectId', $projectId);
+        $noOfProducts = $qb6->getQuery()->getSingleScalarResult(); 
+
+        //Count number of products in this project which already finished
+        
+        $qb = $em->createQueryBuilder();
+        $qb->select('count(distinct pd.id)')
+           ->from('TSProjProductBundle:Product','pd')
+           ->innerJoin('pd.project', 'pj')   
+           ->innerJoin('pd.productStatus', 's')  
+           ->where('pj.id = :projectId')  
+           ->andWhere('s.id = 4')
+           ->setParameter('projectId', $projectId);
+        $noOfFinishedProducts = $qb->getQuery()->getSingleScalarResult(); 
+        
+        $projectFinished = 0;
+        $ProjectStatus = $project->getProjectStatus();
+      
+        
+        if ($ProjectStatus == 'เสร็จสิ้น')
+        {
+            $projectFinished = 1;
+        }     
+        else
+        if ($noOfFinishedProducts ==  $noOfProducts)
+        {
+            $projectFinished = 1;
+        }
+                 
+        $projPercent = $this->percentFinishedCalculation($projTimeConsuming, $projEsDay, $projEsHour, $projEsMin, $projectFinished);
+        $project-> setPercentFinished($projPercent);
+        
+        $em->persist($project); 
+        $em->persist($product);
+        $em->flush();
+        }
+    }
 
 }
